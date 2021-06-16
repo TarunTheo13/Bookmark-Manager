f6bda41f5b0430a576a46d37bdc2b24a0c9ed957
Tarun/Theo Weisz <theo.weisz@gmail.com>
2021-06-16 15:17:15 +0100

    set up test environment

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 feature 'Viewing bookmarks' do
   scenario 'visiting the index page' do
     visit('/')
@@ -9,6 +7,13 @@ end
 
 feature 'Viewing bookmarks' do
   scenario 'A user can see bookmarks' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+
+    # Add the test data
+    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
+
     visit('/bookmarks')
 
     expect(page).to have_content 'http://www.makersacademy.com'
